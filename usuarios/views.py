1d5e4185b9f911be69ce36ec6fc10b7832397486
ClaudioFritzen <sdfritzen96@gmail.com>
2023-04-10 23:32:16 +0100

    criamos o fluxo da aplicação

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return HttpResponse('Hello World')
+        return render(request, 'cadastro.html')
