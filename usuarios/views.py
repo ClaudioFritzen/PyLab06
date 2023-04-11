b743be90558e9d3b165891e57b5245d92b59f0a1
ClaudioFritzen <sdfritzen96@gmail.com>
2023-04-11 18:35:34 +0100

    configuramos os arquivos estaticos

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
-        return HttpResponse('Hello World')
         return render(request, 'cadastro.html')
