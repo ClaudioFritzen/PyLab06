705ac2c3bec666131c39c059a4a4436f19eef231
ClaudioFritzen <sdfritzen96@gmail.com>
2023-04-12 23:24:19 +0100

    criamos as funcionalidades do cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your views here.
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        #### Validações
+
+        if not (senha == confirmar_senha): 
+            messages.add_message(request, constants.ERROR, 'Senhas diferentes')   
+            return redirect(reverse('cadastro'))
+        
+        ## Fazer a validação de força da senha 
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuario já existe')
+            return redirect(reverse('cadastro'))   
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Usuario cadastrado com sucesso!')
+        user.save()
+        return redirect(reverse('login'))
+    
+
+def login(request):
+    pass
